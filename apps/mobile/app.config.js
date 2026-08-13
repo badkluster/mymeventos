86188333892bb9c087a5695818b333b3e46ade9f
Jorge Dominguez <cabronator_pesuti@hotmail.com>
2026-08-13 09:43:52 -0300

    chore(mobile): link EAS Update project

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -1,15 +1,25 @@
 module.exports = ({ config }) => ({
   ...config,
+
   runtimeVersion: {
     policy: 'appVersion'
   },
+
   updates: {
     ...config.updates,
+    url: 'https://u.expo.dev/8268a26a-bc2c-4377-846b-6c5f7a2f5757',
     checkAutomatically: 'NEVER',
     fallbackToCacheTimeout: 0
   },
+
   extra: {
     ...config.extra,
+
+    eas: {
+      ...config.extra?.eas,
+      projectId: '8268a26a-bc2c-4377-846b-6c5f7a2f5757'
+    },
+
     appUpdates: {
       enabled: true,
       appStoreCountry: 'AR',
